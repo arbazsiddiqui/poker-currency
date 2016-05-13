225a16fad30739cdf7381c9bec3bfdc38851552a
arbazsiddiqui <arbaz00@gmail.com>
2016-05-13 12:18:37 +0530

    Basic Structure

---
+++ app.js
@@ -0,0 +1,54 @@
+var players = new Firebase('https://pokercurrency.firebaseio.com/players');
+var pot = new Firebase('https://pokercurrency.firebaseio.com/pot');
+
+function addPlayers() {
+    var playerName = document.getElementById('playerName').value.trim()
+    var amount = document.getElementById('amount').value.trim()
+    players.push({
+        name: playerName,
+        amount: amount
+    });
+}
+
+players.on("value", function (snapshot) {
+    var data = snapshot.val();
+    var list = [];
+    for (var key in data) {
+        name = data[key].name;
+        amount = data[key].amount;
+        list.push({
+            name: name,
+            amount: Number(amount),
+            key: key
+        })
+    }
+    refreshUI(list);
+});
+
+pot.on("value", function(snapshot){
+   var data = snapshot.val();
+   document.getElementById('pot').innerHTML = data.value;
+});
+
+function refreshUI(list) {
+    var lis = '';
+    for (var i = 0; i < list.length; i++) {
+        lis += '<li class="list-group-item" data-key=' + list[i].key + '>' + list[i].name + '   ' + list[i].amount + ' [' + genLinks(list[i].key, list[i].name) + ']'+'</li>';
+    }
+    document.getElementById('playing').innerHTML = lis;
+
+}
+
+function genLinks(key, name) {
+    var links = '';
+    links += '<a href="javascript:bet(\'' + key + '\',\'' + name + '\')">Bet</a> |   ';
+    links += '<a href="javascript:givePot(\'' + key + '\',\'' + name + '\')">Give pot</a>';
+    return links;
+}
+
+function reset(){
+    players.remove();
+    pot.update({
+        value : 0
+    })
+}
